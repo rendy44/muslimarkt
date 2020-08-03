a663ef04d200e29928e9b8bfce834b9c11c67507
Rendy <rendy.de.p@gmail.com>
2020-08-03 21:43:49 +0700

    Add logout url

---
+++ pages/keluar.jsx
@@ -0,0 +1,18 @@
+import Router from 'next/router';
+import {useContext, useEffect} from "react";
+import UserContext from "../components/global/userContext";
+
+export default function PageKeluar() {
+    const {signOut} = useContext(UserContext);
+
+    useEffect(() => {
+        signOut();
+
+        Router.push('/');
+    });
+
+    return (
+        <>
+        </>
+    )
+}
