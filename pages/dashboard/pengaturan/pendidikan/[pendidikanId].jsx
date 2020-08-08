49199c82c00f58f33f395de3190174debf965c6a
Rendy <rendy.de.p@gmail.com>
2020-08-08 08:55:12 +0700

    Add edit study page

---
+++ pages/dashboard/pengaturan/pendidikan/[pendidikanId].jsx
@@ -0,0 +1,13 @@
+import {useRouter} from 'next/router';
+import {DashboardSettingLayout} from "../../../../components/dashboard";
+import PendidikanForm from "../../../../components/form/pendidikan";
+
+export default function PageEditPendidikan() {
+    let router = useRouter();
+    let {pendidikanId} = router.query;
+    return (
+        <DashboardSettingLayout title={'Sunting Pendidikan'}>
+            <PendidikanForm pendidikanId={pendidikanId}/>
+        </DashboardSettingLayout>
+    )
+}
