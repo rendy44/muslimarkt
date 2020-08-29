caf51001dd055c041a79b48a7f80cbac40f9b189
Rendy <rendy.de.p@gmail.com>
2020-08-29 10:48:00 +0700

    Add delete experience functionality

--- components/dashboard/pengalaman.jsx
+++ components/dashboard/pengalaman.jsx
@@ -1,8 +1,13 @@
+import React, {useState, useContext} from "react";
 import PropTypes from 'prop-types';
 import Styles from './pengalaman.module.scss';
 import {LinkBtn, Btn} from "../form";
 import {IoMdSettings, IoMdTrash} from 'react-icons/io';
 import {ListAction} from "./index";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import Experience from "../../src/experience";
+import UserContext from "../global/userContext";
 
 function Experiences(props) {
     const expContent = <div className={Styles.experiences}>{props.children}</div>;
@@ -23,35 +28,87 @@ Experiences.propTypes = {
 };
 
 function ExperienceItem(props) {
-    const usedDateEnd = props.dateEnd ? props.dateEnd : 'sekarang';
-    return (
-        <div className={Styles.experience}>
-            <div className={Styles.inner}>
-                <div className={Styles.edit}>
-                    <LinkBtn
-                        href={'/dashboard/pengaturan/pengalaman/[pengalamanSlug]'}
-                        as={'/dashboard/pengaturan/pengalaman/' + props.slug}
-                        label={''}
-                        icon={<IoMdSettings/>}
-                        variant={'warning'}/>
-                    <Btn label={''} variant={'danger'} isSubmit={false} icon={<IoMdTrash/>}/>
-                </div>
-                <h3>{props.position}</h3>
-                <p className={Styles.itemCompany}>{props.companyName} <span>{props.companyLocation}</span></p>
-                <p className={Styles.itemPeriod}>{props.dateStart} - {usedDateEnd}</p>
-                <div className={Styles.companyDetail}>
-                    <p>
-                        <span>Industri:</span>
-                        <span>{props.companyIndustry}</span>
-                    </p>
-                    <p>
-                        <span>Jabatan:</span>
-                        <span>{props.role}</span>
-                    </p>
+    const {userKey} = useContext(UserContext);
+    const [isDeleted, setIsDeleted] = useState(false);
+
+    const onDelete = (slug) => {
+        const delSwal = withReactContent(Swal);
+        delSwal.fire({
+            text: 'Data yang sudah dihapus tidak bisa dikembalikan lagi',
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonColor: '#3085d6',
+            confirmButtonColor: '#d33',
+            confirmButtonText: 'Hapus',
+            cancelButtonText: 'Batal',
+            allowEscapeKey: false,
+            allowOutsideClick: false
+        }).then((result) => {
+            if (result.value) {
+
+                // Process deleting.
+                Experience.delete(slug, userKey)
+                    .then(result => {
+                        let swalData = {
+                            text: result.data.data,
+                            icon: 'error'
+                        }
+
+                        // Validate result.
+                        if (result.data.success) {
+                            swalData.icon = 'success'
+
+                            setIsDeleted(true)
+                        }
+
+                        // Trigger alert.
+                        delSwal.fire(swalData)
+                    })
+                    .catch(err => {
+                        delSwal.fire({
+                            icon: 'error',
+                            text: err.message
+                        })
+                    })
+            }
+        })
+    }
+
+    if (isDeleted) {
+        return (<></>)
+    } else {
+        const usedDateEnd = props.dateEnd ? props.dateEnd : 'sekarang';
+        return (
+            <div className={Styles.experience}>
+                <div className={Styles.inner}>
+                    <div className={Styles.edit}>
+                        <LinkBtn
+                            href={'/dashboard/pengaturan/pengalaman/[pengalamanSlug]'}
+                            as={'/dashboard/pengaturan/pengalaman/' + props.slug}
+                            label={''}
+                            icon={<IoMdSettings/>}
+                            variant={'warning'}/>
+                        <Btn label={''} variant={'danger'} isSubmit={false} icon={<IoMdTrash/>} onClick={() => {
+                            onDelete(props.slug)
+                        }}/>
+                    </div>
+                    <h3>{props.position}</h3>
+                    <p className={Styles.itemCompany}>{props.companyName} <span>{props.companyLocation}</span></p>
+                    <p className={Styles.itemPeriod}>{props.dateStart} - {usedDateEnd}</p>
+                    <div className={Styles.companyDetail}>
+                        <p>
+                            <span>Industri:</span>
+                            <span>{props.companyIndustry}</span>
+                        </p>
+                        <p>
+                            <span>Jabatan:</span>
+                            <span>{props.role}</span>
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 ExperienceItem.propTypes = {
--- src/experience.js
+++ src/experience.js
@@ -5,16 +5,16 @@ export default class Experience {
         return new Connector('experience/' + key, 'post', param)
     }
 
-    static login(param) {
-        return new Connector('login', 'post', param)
-    }
-
     static get(key) {
         return new Connector('experience/' + key, 'get')
     }
 
     static detail(slug, key) {
         return new Connector('experience/' + slug + '/' + key, 'get')
+    }
+
+    static delete(slug, key) {
+        return new Connector('experience/' + slug + '/' + key, 'delete')
     }
 
     static update(param) {
